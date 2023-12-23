315527c4dd9c3a66346c04ccf963c7cf846042d3
Hayat-07 <hayat1607207@gmail.com>
2023-12-23 12:48:00 +0600

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './assets/pages/Home'
+import Signin from './assets/pages/Signin'
+import SignOut from './assets/pages/SignOut'
+import Profile from './assets/pages/Profile'
+import About from './assets/pages/About'
 
 function App() {
   return (
-    <div className=' '>App
-
-<h2>hook</h2>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/signout" element={<SignOut/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
     
   )
 }
